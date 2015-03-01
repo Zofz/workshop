7a517a83dd828f86071de411d3b1dbf2f4d307ef
Prelang Builder <builder@prelang.com>
2015-03-01 17:39:57 +0000

    Installed relation 'has_and_belongs_to_many' on model 'Part'

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,4 +2,5 @@ class Part < ActiveRecord::Base
   has_and_belongs_to_many :machines
   has_and_belongs_to_many :machine_types
   has_and_belongs_to_many :work_types
+  has_and_belongs_to_many :work_parts
 end
