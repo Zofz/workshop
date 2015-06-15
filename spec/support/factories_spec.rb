cb191da479108335d42498dccc96601e4c95180c
davidwessman <Davidwessman@live.se>
2015-06-15 07:25:28 +0200

    Hound fixes

--- spec/support/factories_spec.rb
+++ spec/support/factories_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "Factory Girl" do
       it "is valid" do
         factory = FactoryGirl.build(factory_name)
         if factory.respond_to?(:valid?)
-          expect(factory).to be_valid,
-            lambda { factory.errors.
-                     full_messages.join('\n') }
+          expect(factory).to be_valid, lambda {
+            factory.errors.full_messages.join('\n')
+          }
         end
       end
 
@@ -18,9 +18,9 @@ RSpec.describe "Factory Girl" do
           it "is valid" do
             factory = FactoryGirl.build(factory_name, trait_name)
             if factory.respond_to?(:valid?)
-              expect(factory).to be_valid,
-                lambda { factory.errors.
-                         full_messages.join('\n') }
+              expect(factory).to be_valid, lambda {
+                factory.errors.full_messages.join('\n')
+              }
             end
           end
         end
