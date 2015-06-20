942f32ca8f4a194d8ba713d71fe13c95fdc78048
davidwessman <Davidwessman@live.se>
2015-06-20 22:35:46 +0200

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations"
+    sessions: "user/sessions",
+    registrations: "user/registrations"
   }
   resource :user, path: :anvandare
 
